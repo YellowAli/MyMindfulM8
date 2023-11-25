1cbdf1d226b88cb8d62356a3289747ab628d7c0e
GusMahfoud <gmahfoud@uwo.ca>
2023-11-25 16:54:59 -0500

    Login 1

---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [password, setPassword] = useState('');
+    const [userName, setUserName] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(phoneNumber);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Register</h2>
+            <form className='register-form' onSubmit={handleSubmit}>
+                <label htmlFor="userName">Username</label>
+                <input value={userName} onChange={(e) => setUserName(e.target.value)} type="text" placeholder="User Name" id="userName" name="userName"/>
+                <label htmlFor="password">Password</label>
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="*****" id="password" name="password"/>
+                <label htmlFor="phoneNumber">Phone Number</label>
+                <input value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} type="tel" placeholder="+_ ( _ _ _) ..." id="phoneNumber" name="phoneNumber"/>
+                <button type="submit">Register</button>
+            </form>
+            <button className='register-link' onClick={() => navigate('/login')}>Already have an Account? Login Here.</button>
+        </div> 
+    );
+}
+
+export default Register;
