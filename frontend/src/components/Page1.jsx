d09b28c40b3576199d2d65a9ad0296ad2ec36621
GusMahfoud <gmahfoud@uwo.ca>
2023-11-26 01:23:51 -0500

    pg 11

--- frontend/src/components/Page1.jsx
+++ frontend/src/components/Page1.jsx
@@ -11,18 +11,39 @@ function Page1() {
         "You are stronger than you think.",
         "Keep pushing forward.",
         "Take it one step at a time."
-        // Add more messages as you see fit
     ];
+
+    const supportMessages2 = [
+        "Meditate For 5 minutes",
+        "Afternoon Walk or Workout",
+        "10 Minutes of Mindful Reflection",
+        "10 Minutes of Social Media Detox",
+        "Make Bed",
+    ]
 
     // State to hold the current support message
     const [currentSupportMessage, setCurrentSupportMessage] = useState('');
-
+    const [currentSupportMessage2, setCurrentSupportMessage2] = useState('');
+    const [currentSupportMessage3, setCurrentSupportMessage3] = useState('');
+    const [currentSupportMessage4, setCurrentSupportMessage4] = useState('');
     // Function to select a random support message
     useEffect(() => {
         const randomIndex = Math.floor(Math.random() * supportMessages.length);
         setCurrentSupportMessage(supportMessages[randomIndex]);
     }, []); // The empty array ensures this effect runs only once when the component mounts
 
+    useEffect(() => {
+        const randomIndex = Math.floor(Math.random() * supportMessages2.length);
+        setCurrentSupportMessage2(supportMessages2[randomIndex]);
+    }, []);
+    useEffect(() => {
+        const randomIndex = Math.floor(Math.random() * supportMessages2.length);
+        setCurrentSupportMessage3(supportMessages2[randomIndex]);
+    }, []);
+    useEffect(() => {
+        const randomIndex = Math.floor(Math.random() * supportMessages2.length);
+        setCurrentSupportMessage4(supportMessages2[randomIndex]);
+    }, []);
     return (
         <div className="container">
             <div className="header-container">Your current focus is:</div>
@@ -34,21 +55,33 @@ function Page1() {
             </div>
             <div className="support">{currentSupportMessage}</div>
             <ul class="list-group">
-            <li class="list-group-item">An item</li>
-            <el class = "message">2</el>
-            <el class = "message">1</el>
-            <li class="list-group-item">A second item</li>
-            <el class = "message">3</el>
-            <el class = "message">2</el>
-            <li class="list-group-item">A third item</li>
-            <el class = "message">1</el>
-            <el class = "message">2</el>
-            <li class="list-group-item">A fourth item</li>
-            <el class = "message">4</el>
-            <el class = "message">4</el>
-            <li class="list-group-item">And a fifth one</li>
-            <el class = "message">2</el>
-            <el class = "message">1</el>
+            <li class="list-group-item">Affirmations</li>
+            <el class = "message">Affirmations, when practiced consistently, have been shown to decrease health-deteriorating stress, can be beneficial in significantly improving optimism and feelings of hope, 
+            and may also aid in the mitigation of the effects of negative emotional bias, a common feature in depressive disorders. </el>
+            <li class="list-group-item">Journaling</li>
+            <el class = "message">Journaling has been associated with reduced stress and improved 
+            mood by helping individuals process emotions and increase self-awareness</el>
+            <li class="list-group-item">Deep Breathing</li>
+            <el class = "message">Deep breathing techniques have been linked to decreased anxiety and 
+            an enhanced relaxation response through the activation of the parasympathetic nervous system </el>
+            <li class="list-group-item">Goals</li>
+            <el class = "message">
+            <div>
+                <input type="checkbox" id="goal1" name="goal1" />
+                <label for="goal1">{currentSupportMessage2}</label>
+            </div>
+            <div>
+                <input type="checkbox" id="goal2" name="goal2" />
+                <label for="goal2">{currentSupportMessage3}</label>
+            </div>
+            <div>
+                <input type="checkbox" id="goal3" name="goal3" />
+                <label for="goal3">{currentSupportMessage4}</label>
+            </div>
+            </el>
+            <li class="list-group-item">Gratitude</li>
+            <el class = "message">Expressing gratitude consistently is correlated with long-term well-being, reduced depression,
+             and increased happiness, fostering a positive mindset and stronger resilience </el>
             </ul>
         </div>
     );
